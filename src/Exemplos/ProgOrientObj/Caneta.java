7bc1cc4957a1bcd306025c906a6f6e2c8c5f99d2
Danilo <danilo.volles@outlook.com>
2022-10-31 21:51:22 -0300

    POO - Caneta

--- src/Exemplos/ProgOrientObj/Caneta.java
+++ src/Exemplos/ProgOrientObj/Caneta.java
@@ -1,22 +1,58 @@
 package Exemplos.ProgOrientObj;
 
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampa;
+    private String modelo;
+    private String cor;
+    private int carga;
+    private boolean tampa = false;
+
+    // Método construtor
+    Caneta (String modelo, String cor) {
+        tampa = false;
+        this.modelo = modelo;
+        this.cor = cor;
+        carga = 30;
+    }
 
     void rabiscar (){
+        escrever("Rabisco");
+    }
+    public void escrever(String texto) {
         if (tampa) {
-            System.out.println("Rabisco");
+            System.out.println(texto);
+            texto = texto.replaceAll(" ","");
+            if (carga >= texto.length()) {
+                carga -= texto.length();
+            } else {
+                System.out.println("Carga insuficiente");
+            }
         } else {
             System.out.println("Retire a tampa");
         }
     }
 
     void tampar(){
+        tampa = false;
+    }
+
+    void destampar(){
         tampa = true;
+    }
+
+    // Botão direito, generate, to string
+    @Override
+    public String toString() {
+        return "Caneta{" +
+                "modelo='" + modelo + '\'' +
+                ", cor='" + cor + '\'' +
+                ", carga=" + carga +
+                ", tampa=" + tampa +
+                '}';
+    }
+
+    public void printStatus(){
+        System.out.println(toString());
+     // System.out.println(this); // outra forma de chamar o método
     }
 }
 
--- src/Exemplos/ProgOrientObj/Exemplo00.java
+++ src/Exemplos/ProgOrientObj/Exemplo00.java
@@ -3,16 +3,18 @@ package Exemplos.ProgOrientObj;
 public class Exemplo00 {
     public static void main(String[] args) {
         // Criação do objeto do tipo caneta
-        Caneta caneta1 = new Caneta();
-        // Descrição do objeto
-        caneta1.modelo = "BIC";
-        caneta1.cor = "Azul";
-        caneta1.ponta = 0.9f;
-        caneta1.carga = 100;
-        caneta1.tampa = false;
+        Caneta caneta1 = new Caneta("BIC","AZUL");
+        caneta1.printStatus();
         // chamar a caneta para rabiscar
         caneta1.rabiscar();
-        caneta1.tampar();
+        caneta1.printStatus();
+        caneta1.destampar();
         caneta1.rabiscar();
+        caneta1.printStatus();
+        caneta1.escrever("DANILO VOLLES ARAUJO");
+        caneta1.printStatus();
+
+        // Criação de outro objeto
+        Caneta caneta2 = new Caneta("BIC", "Preta");
     }
 }
